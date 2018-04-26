1e67080211dbe367c17ca4b187991a1ad692588f
Jeremy Bell <grcodemonkey@gmail.com>
2018-04-26 12:01:03 -0400

    Added menu affordances and accessibility
    Fixed custom font not loading

--- frontend/index.js
+++ frontend/index.js
@@ -3,12 +3,27 @@
 import { toggleClass } from "./js/class";
 
 window.onload = function() {
-    var menuToggle = document.getElementById("menu-toggle")
-    var menu = document.getElementById("menu")
-    menuToggle.addEventListener("click", function(){
-        toggleClass(menu, "active-menu");
-    });
-}
+  var menuToggle = document.getElementById("menu-toggle");
+  var menu = document.getElementById("menu");
+  var menuOpen = false;
+  function toggleMenu() {
+    toggleClass(menu, "active-menu");
+    menuOpen = !menuOpen;
+    menu.setAttribute("aria-expanded", menuOpen);
+  }
+  menuToggle.addEventListener("click", function() {
+    toggleMenu();
+  });
+  document.body.addEventListener(
+    "click",
+    function() {
+      if (menuOpen) {
+        toggleMenu();
+      }
+    },
+    true
+  );
+};
 
 // CSS
 require("./styles/main.scss");
