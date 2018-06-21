4162d328e8127244c6160e499d8217cb0c6aa44c
CJ Davis <cj@cjdavis.me>
2018-06-20 21:40:18 -0400

    Initial setup of GTM script based on environment variable configuration

--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using CodeForGood.Config;
 
 namespace CodeForGood
 {
@@ -30,6 +31,8 @@ namespace CodeForGood
             services.AddRouting(options => {
                 options.LowercaseUrls = true;
             });
+
+            services.AddSingleton<IGoogleSettings, GoogleSettings>(e => Configuration.GetSection(nameof(GoogleSettings)).Get<GoogleSettings>());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
